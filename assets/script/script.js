8a36b201ad514000eca3934b72cf74a9c2cb0e23
ChristieHyde <christieahyde@gmail.com>
2023-11-02 19:58:41 +1100

    Improved password building algorithm by adding percentage based character picker

--- assets/script/script.js
+++ assets/script/script.js
@@ -40,25 +40,59 @@ function generatePassword() {
     }
 
     // Collate the pool of available characters to use in the password
-    var characterPool = [];
+    var fullCharacterPool = [];
     if (includeLower) {
-        characterPool = characterPool.concat(lowerCasedCharacters)
+        fullCharacterPool = fullCharacterPool.concat(lowerCasedCharacters)
     }
     if (includeUpper) {
-        characterPool = characterPool.concat(upperCasedCharacters)
+        fullCharacterPool = fullCharacterPool.concat(upperCasedCharacters)
     }
     if (includeNumber) {
-        characterPool = characterPool.concat(numericCharacters)
+        fullCharacterPool = fullCharacterPool.concat(numericCharacters)
     }
     if (includeSpecial) {
-        characterPool = characterPool.concat(specialCharacters)
+        fullCharacterPool = fullCharacterPool.concat(specialCharacters)
     }
+
+    // Define variables to track if the character types appear in the password
+    var lowerIsIn = false;
+    var upperIsIn = false;
+    var numberIsIn = false;
+    var specialIsIn = false;
 
     // Build password from available characters
     var password = ""
     for (var i = 0; i < passwordLength; i++) {
-        nextCharacter = selectCharacter(characterPool);
+
+        // Choose either the general character pool or a specialist pool to pick a character from
+        var pool;
+        var offset = 0;
+        if (includeLower && !lowerIsIn && pickSpecialistPool(password.length, passwordLength, ++offset)) {
+            pool = lowerCasedCharacters;
+        } else if (includeUpper && !upperIsIn && pickSpecialistPool(password.length, passwordLength, ++offset)) {
+            pool = upperCasedCharacters;
+        } else if (includeNumber && !numberIsIn && pickSpecialistPool(password.length, passwordLength, ++offset)) {
+            pool = numericCharacters;
+        } else if (includeSpecial && !specialIsIn && pickSpecialistPool(password.length, passwordLength, ++offset)) {
+            pool = specialCharacters;
+        } else {
+            pool = fullCharacterPool;
+        }
+
+        // Select a character from the chosen pool to add to the password
+        nextCharacter = selectCharacter(pool);
         password = password.concat(nextCharacter);
+
+        // Record the type of character that was selected if it is new to the password
+        if (lowerCasedCharacters.find(chara => chara === nextCharacter)) {
+            lowerIsIn = true;
+        } else if (upperCasedCharacters.find(chara => chara === nextCharacter)) {
+            upperIsIn = true;
+        } else if (numericCharacters.find(chara => chara === nextCharacter)) {
+            numberIsIn = true;
+        } else if (specialCharacters.find(chara => chara === nextCharacter)) {
+            specialIsIn = true;
+        }
     }
 
     return password;
@@ -68,4 +102,12 @@ function generatePassword() {
 function selectCharacter(pool) {
     var x = Math.floor(Math.random() * pool.length);
     return pool[x];
+}
+
+// Decide whether a specialist character pool should be chosen (percentage chance system)
+function pickSpecialistPool(currentLength, totalLength, offset) {
+    var percentChance = (currentLength+offset)/totalLength;
+    var choice = Math.random();
+    console.log(currentLength + " " + percentChance + " " + choice);
+    return (choice <= percentChance);
 }
